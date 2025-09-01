bc43800b7f9174272908f34c4bdc9e0ecd136bb3
alexandermehks <alexander@mehks.se>
2025-09-01 06:44:39 +0200

    delete git branch script

--- scripts/startup_scripts/change_mouse_speed.sh
+++ scripts/startup_scripts/change_mouse_speed.sh
@@ -4,5 +4,5 @@
 ###
 # RUN: xinput list to find your device/mouse name.
 ###
-xinput --set-prop 'pointer:''Razer Razer DeathAdder Chroma' 'libinput Accel Profile Enabled' 0, 1
-xinput --set-prop 'pointer:''Razer Razer DeathAdder Chroma' 'libinput Accel Speed' -0.5
+xinput --set-prop 'pointer:''Logitech G Pro' 'libinput Accel Profile Enabled' 0, 1
+xinput --set-prop 'pointer:''Logitech G Pro' 'libinput Accel Speed' -0.5
